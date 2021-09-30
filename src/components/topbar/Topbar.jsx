2df54a41ad1bd72dbdcf6a91543cd16d11c09620
田原 一樹 <kazuki.tahara@rarejob.co.jp>
2021-09-30 10:50:40 +0900

    update

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './topbar.css'
+
+const Topbar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i className="topIcon fab"></i>
+        <i className="topIcon fab"></i>
+        <i className="topIcon fab"></i>
+        <i className="topIcon fab"></i>
+      </div>
+      <div className="topCenter">
+
+      </div>
+      <div className="topRight">
+
+      </div>
+      
+    </div>
+  )
+}
+
+export default Topbar
