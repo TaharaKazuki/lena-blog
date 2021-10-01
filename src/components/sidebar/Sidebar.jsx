ca1335861daa5ec5b98940115b98001172cdc5f0
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-01 21:23:15 +0900

    update

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,56 @@
+import { Link } from 'react-router-dom'
+import './sidebar.scss'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__item">
+        <span className="sidebar__title">ABOUT ME</span>
+        <img
+          src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+          alt="sampleImage"
+        />
+        <p>
+          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+          amet ex esse.Sunt eu ut nostrud id quis proident.
+        </p>
+      </div>
+      <div className="sidebar__item">
+        <span className="sidebar__title">CATEGORIES</span>
+        <ul className="sidebar__list">
+          <li className="sidebar__listItem">
+            <Link className="link" to="/posts?cat=Life">
+              Life
+            </Link>
+          </li>
+          <li className="sidebar__listItem">
+            <Link className="link" to="/posts?cat=Music">
+              Music
+            </Link>
+          </li>
+          <li className="sidebar__listItem">
+            <Link className="link" to="/posts?cat=Sport">
+              Sport
+            </Link>
+          </li>
+          <li className="sidebar__listItem">
+            <Link className="link" to="/posts?cat=Style">
+              Style
+            </Link>
+          </li>
+          <li className="sidebar__listItem">
+            <Link className="link" to="/posts?cat=Cinema">
+              Cinema
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebar__item">
+        <span className="sidebar__title">FOLLOW US</span>
+        <div className="sidebar__social"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
