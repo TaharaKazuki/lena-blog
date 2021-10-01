7ccf888d6440d49a4770c40ec289715ff451bbfb
田原 一樹 <ssp0727.lnc@gmail.com>
2021-10-01 21:45:39 +0900

    update

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,8 @@
-import { useLocation } from "react-router";
+import { useLocation } from 'react-router'
 import Header from '../../components/header/Header'
-import Posts from "../../components/posts/Posts";
+import Posts from '../../components/posts/Posts'
+import Sidebar from '../../components/sidebar/Sidebar'
+import './homepage.scss'
 
 const Homepage = () => {
   const location = useLocation()
@@ -11,6 +13,7 @@ const Homepage = () => {
       <Header />
       <div className="home">
         <Posts />
+        <Sidebar />
       </div>
     </>
   )
